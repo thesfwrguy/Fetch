7309372b8733f04404fa2dd7f4d1b1baadf2f46f
forkbattery <forkbattery@gmail.com>
2014-12-15 22:45:14 -0500

    first commit

---
+++ Fetch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fetch
+//
+//  Created by Kanishka Goel on 2014-12-15.
+//  Copyright (c) 2014 Kanishka Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
