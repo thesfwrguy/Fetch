91e5203f9cf95f7e33f5a2005eb320d658942abc
forkbattery <forkbattery@gmail.com>
2014-12-16 03:23:24 -0500

    Added UI elements in code and connected

--- Fetch/ViewController.swift
+++ Fetch/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleLabel        : UILabel?      //movie title
+    @IBOutlet var releasedLabel     : UILabel?      //movie release date
+    @IBOutlet var ratingLabel       : UILabel?      //movie rating
+    @IBOutlet var plotLabel         : UILabel?      //movie plot
+    @IBOutlet var posterImageView   : UIImageView?  //movie poster
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        self.titleLabel!.text       = "Interstellar"
+        self.releasedLabel!.text    = "2014"
+        self.ratingLabel!.text      = "PG"
+        self.plotLabel!.text        = "A team of explorers travel through a wormhole in an attempt to ensure humanity's survival"
+        self.posterImageView!.image = UIImage(named: "Interstellar.jpg")
+        
+    }
+    
 
 }
 
