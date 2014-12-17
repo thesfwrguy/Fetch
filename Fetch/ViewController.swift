84e8f135c875c89cb367ab9af539a8b38cb979ca
forkbattery <forkbattery@gmail.com>
2014-12-17 00:52:23 -0500

    Setup API request and response

--- Fetch/ViewController.swift
+++ Fetch/ViewController.swift
@@ -27,11 +27,61 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonPressed(sender: AnyObject) {
-        self.titleLabel!.text       = "Interstellar"
-        self.releasedLabel!.text    = "2014"
-        self.ratingLabel!.text      = "PG"
-        self.plotLabel!.text        = "A team of explorers travel through a wormhole in an attempt to ensure humanity's survival"
-        self.posterImageView!.image = UIImage(named: "Interstellar.jpg")
+        self.searchIMDb("King of Kong")
+        
+    }
+    
+    func searchIMDb(forContent : String) {
+        
+        //clean the string of spaces and get it ready to pass onto the API
+        //will replace spaces in the string with %20 which is the ascii encoded value of a space in a URL string
+        
+        var spacelessString = forContent.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        
+        if let foundString = spacelessString? {
+            
+            //create NSURL object - to pass onto
+            var urlPath = NSURL(string: "http://www.omdbapi.com/?t=\(foundString)")
+    
+            //to reach out to the internet, we need sharedsession - a singleton object that can be returned from the NSURLSession (provides an API for downloading content via HTTP
+            var session = NSURLSession.sharedSession()
+        
+            //making our API request by calling data task by passing URL on that session
+            var task = session.dataTaskWithURL(urlPath!) {
+            
+                data, response, error -> Void in
+            
+                //print the error if any exist
+                if (error != nil) {
+                    println(error.localizedDescription)
+                }
+                
+                //create an NSError optional object to pass in as parameter
+                var jsonError : NSError?
+                
+                //take the 'data' that was passed in from the dataTaskWithURL request and put it in to a variable
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as Dictionary<String,String>
+                
+                if(jsonError != nil) {
+                    println(jsonError!.localizedDescription)
+                }
+                
+                
+                
+               // if let apiDelegate = self.delegate? {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        
+                        self.titleLabel?.text       = jsonResult["Title"]
+                        self.releasedLabel?.text    = jsonResult["Released"]
+                        self.ratingLabel?.text      = jsonResult["Rated"]
+                        self.plotLabel?.text        = jsonResult["Plot"]
+                        //apiDelegate.didFinishIMDbSearch(jsonResult)
+                    }
+                //}
+            
+            }
+            task.resume()
+        }
         
     }
     
